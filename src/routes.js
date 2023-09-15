68b00b27b3b9f5c421de597762389c1fd5946f96
Patritsiya Georgieva <patritsiyag@vmware.com>
2023-09-16 00:07:54 +0300

    add about page and fix nav

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
     const express = require('express');
     const homeController =require('./controllers/homeControllers');
+    const cubeController =require('./controllers/cubeController');
 
     const router = express.Router();
     router.get('/', homeController.index);
-    module.exports = router;
+    router.get('/about', homeController.about);
     
+    router.use('/cube', cubeController);
+    module.exports = router;
