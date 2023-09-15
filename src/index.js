8680ae1732bef5a8e552fe7c6f1d5136c32c7e26
Patritsiya Georgieva <patritsiyag@vmware.com>
2023-09-15 23:20:54 +0300

    initial set up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 app.get('/', (req, res) => {
-    res.send('AAA')
+    res.render('index')
 });
 
 app.listen(5000, () => console.log('App is listening on post 5000'));
