9cf645e8dd5623f76828cd9196eae1e51d6f3793
Patritsiya Georgieva <patritsiyag@vmware.com>
2023-09-16 15:20:47 +0300

    implement search

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,7 +1,12 @@
 const cubes = require('../db.json');
 const router = require('express').Router();
+const cubeService = require('../services/cubeService');
 
 router.get('/', (req, res) => {
+    const { search, from, to} = req.query;
+
+    const cubes = cubeService.getAll(search, from, to);
+
     res.render('index', { cubes });
 });
 router.get('about', (req, res) => {
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -4,10 +4,18 @@ const path = require('path');
 
 exports.save = (cube) => {
 
-       cubes.push({...cube, id: cubes[cubes.length - 1].id + 1} );
-      return fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {encoding: 'utf-8'});
-} 
+    cubes.push({ ...cube, id: cubes[cubes.length - 1].id + 1 });
+    return fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), { encoding: 'utf-8' });
+}
 
 exports.getOne = (cubeId) => {
-   return cubes[cubeId];
-} 
+    return cubes[cubeId];
+}
+exports.getAll = (search, from, to) => {
+    const result =  cubes
+    .filter(x => x.name?.toLowerCase().includes(search?.toLowerCase() || ''))
+    .filter(x => from ? x.difficultyLevel >= from : true)
+    .filter(x => to ? x.difficultyLevel <= to : true)
+
+    return result
+}
